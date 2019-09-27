7e8ec2ded78c62774edc0d8ed4975687933f2d18
Tham <thamtham1711.tt@gmail.com>
2019-09-27 10:06:26 +0700

    unknow

--- NewsApp/App/Containers/SourceContainer.js
+++ NewsApp/App/Containers/SourceContainer.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+<<<<<<< Updated upstream
 import { View, Button } from 'react-native'
 import AppText from '../Components/Common/AppText'
 
@@ -10,6 +11,93 @@ export default class SourceContainer extends Component {
                 <AppText>Source Screen</AppText>
                 <Button title='Move to Article Container' onPress={()=>{this.props.navigation.navigate('ArticleScreen')}} />
                 <Button title='Go Back' onPress={()=>{this.props.navigation.goBack()}} />
+=======
+import { View, Button, FlatList, Text } from 'react-native'
+import styles from './Styles/SourceContainerStyle'
+import AppText from '../Components/Common/AppText'
+import SourceItem from '../Components/Source/SourceItem';
+import AppFlatList from '../Components/Common/AppFlatList'
+
+
+export default class SourceContainer extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            listResource: [{
+                id: "associated-press",
+                name: "Associated Press",
+                description: "The AP delivers in-depth coverage on the international, politics, lifestyle, business, and entertainment news.",
+                url: "https://apnews.com/",
+                category: "general",
+                language: "en",
+                country: "us",
+                urlsToLogos: {
+                    small: "",
+                    medium: "",
+                    large: ""
+                },
+            },
+            {
+                id: "associated-press",
+                name: "Associated Press",
+                description: "The AP delivers in-depth coverage on the international, politics, lifestyle, business, and entertainment news.",
+                url: "https://apnews.com/",
+                category: "general",
+                language: "en",
+                country: "us",
+                urlsToLogos: {
+                    small: "",
+                    medium: "",
+                    large: ""
+                },
+            },
+            {
+                id: "associated-press",
+                name: "Associated Press",
+                description: "The AP delivers in-depth coverage on the international, politics, lifestyle, business, and entertainment news.",
+                url: "https://apnews.com/",
+                category: "general",
+                language: "en",
+                country: "us",
+                urlsToLogos: {
+                    small: "",
+                    medium: "",
+                    large: ""
+                },
+            },
+            {
+                id: "associated-press",
+                name: "Associated Press",
+                description: "The AP delivers in-depth coverage on the international, politics, lifestyle, business, and entertainment news.",
+                url: "https://apnews.com/",
+                category: "general",
+                language: "en",
+                country: "us",
+                urlsToLogos: {
+                    small: "",
+                    medium: "",
+                    large: ""
+                },
+            },
+
+            ]
+        }
+    }
+
+
+
+    render() {
+        const {listResource} = this.state
+        const flatListProps = {
+            sourceData: listResource,
+            numColumns: 2
+        }
+        return (
+            <View style={styles.container}>
+                <AppFlatList {...flatListProps} />
+                <Button title='Move to Article Container' onPress={() => { this.props.navigation.navigate('ArticleScreen') }} />
+                <Button title='Go Back' onPress={() => { this.props.navigation.goBack() }} />
+>>>>>>> Stashed changes
             </View>
         )
     }
