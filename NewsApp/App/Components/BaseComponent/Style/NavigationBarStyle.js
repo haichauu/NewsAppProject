4d28d4962e83485b9b150a386fe31f0e153adaf1
Tham <thamtham1711.tt@gmail.com>
2019-10-02 15:26:08 +0700

    Predicted Finish

--- NewsApp/App/Components/BaseComponent/Style/NavigationBarStyle.js
+++
@@ -1,9 +0,0 @@
-import { View, Text, StyleSheet } from 'react-native';
-
-const styles = StyleSheet.create({
-    container: {
-        flexDirection: "row",
-        paddingHorizontal: 15,
-        height: 44
-    }
-})
