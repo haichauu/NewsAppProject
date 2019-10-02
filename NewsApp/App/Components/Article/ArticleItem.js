65313a8dc752dc0ea071c268205bda3416bef63c
Tham <thamtham1711.tt@gmail.com>
2019-10-02 15:18:56 +0700

    add FastImage

--- NewsApp/App/Components/Article/ArticleItem.js
+++ NewsApp/App/Components/Article/ArticleItem.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
-import {View, ImageBackground, TouchableOpacity} from 'react-native';
+import {View, TouchableOpacity} from 'react-native';
 import AppText from '../Common/AppText'
 import styles from './Styles/ArticleItemStyle'
 import MoreLessText from '../Common/MoreLessText'
 import {convertUTCToLocal} from '../../Utils/Utils'
+import FastImage from 'react-native-fast-image'
 
 const ArticleItem = (props) => {
     const {urlImage, title, author, publishedAt, description, style, onPressItem} = props
@@ -19,7 +20,7 @@ const ArticleItem = (props) => {
     return(
 
         <TouchableOpacity {...props} style={[styles.container, style]} onPress={onPressItem} >
-            <ImageBackground source={{uri: urlImage}} style={styles.imageBackground} />
+            <FastImage source={{uri: urlImage}} style={styles.imageBackground} />
             <View style={styles.infoWrapper}>
                 <AppText style={styles.titleText} text={title} />
                 <AppText style={styles.authorText} text={author} />
